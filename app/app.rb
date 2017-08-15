849cfed3fe2db3a50311a61dce7d980829e6f2cb
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2017-08-15 23:29:56 +0900

    POST内容をチャットワークに通知する

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,20 @@ class App < Sinatra::Base
 
   post '/' do
     params = JSON.parse request.body.read
-    @issue = {
+    issue = {
       key:     "#{params['project']['projectKey']}-#{params['content']['key_id']}",
       summary: params['content']['summary'],
       creator: params['createdUser']['name'],
     }
+    Chatwork.new.sendMessage(makeChatworkMessage(issue))
     return true
+  end
+
+  private
+  def makeChatworkMessage(issue)
+    message =  "[info][title]#{issue[:creator]}さんが課題を作成しました[/title]"
+    message += "[#{issue[:key]}] #{issue[:summary]}\n"
+    message += "https://saknight.backlog.jp/view/#{issue[:key]}[/info]"
   end
 
 end
