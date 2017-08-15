aa71eb0caf4f654359745e8fd70ad2092f442bd9
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2017-08-15 23:17:33 +0900

    WebHookから受け取ったデータのうち必要なもののみ取り出すように

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 require 'json'
 require 'pp'
+require_relative 'chatwork'
 
 class App < Sinatra::Base
 
   post '/' do
-    pp JSON.parse request.body.read
+    params = JSON.parse request.body.read
+    @issue = {
+      key:     "#{params['project']['projectKey']}-#{params['content']['key_id']}",
+      summary: params['content']['summary'],
+      creator: params['createdUser']['name'],
+    }
     return true
   end
 
